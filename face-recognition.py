5203170f52622ba67987a2b2671bb40a56e9de53
Mohammad mahdi omraei <152789233+ARMEN696@users.noreply.github.com>
2023-12-24 14:28:47 +0330

    Add files via upload

---
+++ face-recognition.py
@@ -0,0 +1,41 @@
+import cv2
+import cvzone
+from cvzone.FaceMeshModule import FaceMeshDetector
+from cvzone.PlotModule import LivePlot
+
+cap = cv2.VideoCapture(0)
+detector = FaceMeshDetector(maxFaces=1)
+plotY = LivePlot(640, 360, [20, 50], invert=True)
+
+idList = [22, 23, 24, 26, 110, 157, 158, 159, 160, 161, 130, 243]
+ratioList = []
+blinkCounter = 0
+counter = 0
+color = (255, 0, 255)
+
+while True:
+
+   
+
+    success, img = cap.read()
+    img, faces = detector.findFaceMesh(img, draw=True)
+
+    if faces:
+        face = faces[0]
+        for id in idList:
+            cv2.circle(img, face[id], 5,color, cv2.FILLED)
+
+        leftUp = face[159]
+        leftDown = face[23]
+        leftLeft = face[130]
+        leftRight = face[243]
+        lenghtVer, _ = detector.findDistance(leftUp, leftDown)
+        lenghtHor, _ = detector.findDistance(leftLeft, leftRight)
+
+        cv2.line(img, leftUp, leftDown, (0, 200, 0), 3)
+        cv2.line(img, leftLeft, leftRight, (0, 200, 0), 3)
+
+      
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(25)
